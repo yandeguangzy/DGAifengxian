bca552222831f0edf0f4920ed632004a3db41f29
yandeguang <754199572@qq.com>
2016-07-18 11:20:20 +0800

    init

---
+++ aifengxian/aifengxian/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  aifengxian
+//
+//  Created by FSLB on 16/7/18.
+//  Copyright © 2016年 FSLB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
